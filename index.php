62e14fcadd4402a1bc22e3a97720cc92e62bad75
ClareDavidson <clare@davidson.me.uk>
2018-04-04 22:50:55 +0100

    merged index pages.

--- index.php
+++ index.php
@@ -1,17 +1,22 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>MVC Skeleton</title>
     </head>
     <body>
         <?php
-        // put your code here
+    require_once('connection.php');
+        
+    if (isset($_GET['controller']) && isset($_GET['action'])) {
+        $controller = $_GET['controller'];
+        $action     = $_GET['action'];
+  } else {
+        $controller = 'pages';
+        $action     = 'home';
+  }
+
+    require_once('views/layout.php');
         ?>
     </body>
 </html>
