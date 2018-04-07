2d86dd4b3b9072521b8a37711d01458cb6922f84
ClareDavidson <clare@davidson.me.uk>
2018-04-07 14:56:09 +0100

    Created a blogPost model, started the constructor and made a create method

---
+++ models/blogPost.php
@@ -0,0 +1,40 @@
+<?php
+  class BlogPost {
+
+    // we define 3 attributes
+    private $blogPostID;
+    private $title;
+    private $post;
+    private $date;
+    private $keywords;
+
+    public function __construct($blogPostID=NULL) {
+        if ($blogPostID!=NULL){
+            //Gayathri you will need to complete the constructor
+        };
+    }
+    
+    public function create($title, $post, $keywords){
+        try {
+            $this->title = $title;
+            $this->post = $post;
+            $this->keywords = $keywords;
+            $this->date = date('Y-m-d');
+            $pdo = DB::getInstance();
+            $stmt = $pdo->prepare("INSERT INTO blogPost(title, content, datePosted, keywords) VALUES (:title, :post, :datePosted, :keywords)");
+            $statement->execute(array(
+                "title" => "$this->title",
+                "post" => "$this->post",
+                "date" => "$this->date",
+                "keywords" => "$this->keywords"
+                ));
+            $this->blogPostID = $pdo->lastInsertId();
+            return true;
+        }catch (Exception $ex) {
+            return false;
+        }
+    }
+
+        
+
+  }
