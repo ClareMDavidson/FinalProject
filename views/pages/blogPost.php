6fdfae318b681e205366d2725b64ceb81fac2d42
ClareDavidson <clare@davidson.me.uk>
2018-04-07 11:42:53 +0100

    created basic form in views/pages/blogPost.php. Needs styling.

---
+++ views/pages/blogPost.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Blog Post</title>
+    </head>
+    <body>
+        <input type="text" name="Post Title" autofocus="true" placeholder="Title" required>
+        <textarea name="Post" placeholder="Write your blogpost here" required></textarea>
+        <input type="text" name="Keywords" placeholder="Add keywords, seperated by a comma">
+    <buton type="sumbit" name="Submit">Send</buton>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
