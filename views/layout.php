c8107e3cb2ebdd9ca95bf8ff1ea0051d45748bfd
Jess Pearson <jessica.pearson@sky.uk>
2018-04-08 19:37:54 +0100

    Styled Login page
    -Added link to layout header so we can use bootstraps floating labels for forms.
    -Added all styling for log in page to the styles page.
    -Removed all local styling from the login.php page as now all been moved to the styles.css page

--- views/layout.php
+++ views/layout.php
@@ -5,6 +5,8 @@
 
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 <!--link to bootstrap cdn-->
+<link rel="stylesheet" href="https://cdn.rawgit.com/tonystar/bootstrap-float-label/v4.0.0/dist/bootstrap-float-label.min.css"/>
+<!--link to bootstrap floating labels-->
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Amatic+SC|Montserrat|Open+Sans">
 <!--direct link to google fonts-->
 <link rel="stylesheet" href="views/css/styles.css">
--- views/pages/login.php
+++ views/pages/login.php
@@ -1,41 +1,19 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <style>
-            .loginContainer { 
-                              margin: auto;
-                              width: 500px;
-                              height: 500px;   
-            }
-            
-            h1 { text-align: center;}
-            
-            form { padding: 100px 0;
-                text-align: center;}  
-        </style>
-    </head>
-    <body>
-        <div class="loginContainer" >
-            <h1>Log in</h1>
-            <form action="" method="POST">
-                <label for="username"><b>Username</b></label><br>
-                <input type="text" placeholder="Enter Username" name="username" required><br>
-                
-                <label for="password"><b>Password</b></label><br>
-                <input type="password" placeholder="Enter Password" name="password" required><br>
-                
-                <label>
-                <input type="checkbox" checked="checked" name="remember"> Remember me
-                </label><br>
-                
-                <button type="submit">Log in</button><br>  
-            </form>
+
+<div class="form-label-group loginStyle">
+    <form action="" method="POST">
+        <legend align="center"><b>Log in</b></legend>
+        <div class="form-label-group">               
+            <input class="form-control" type="text" placeholder="Enter Username" name="username" autofocus="" required>
+            <label for="username"><b>Username</b></label>
+        </div><br>
+        <div class="form-label-group">                   
+            <input class="form-control" type="password" placeholder="Enter Password" name="password">
+            <label for="password"><b>Password</b></label>
         </div>
-    </body>
-</html>
+        <label>
+        <input type="checkbox" checked="checked" name="remember"> Remember me
+        </label><br>
+        <button class="loginButton form-control hvr-grow hvr-fade" type="submit">Log in</button><br>  
+    </form>
+</div>
+
