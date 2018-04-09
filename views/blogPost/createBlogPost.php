c8d99caf3f9b11885dd8b74805d29048c1fc3559
ClareDavidson <clare@davidson.me.uk>
2018-04-09 15:54:27 +0100

    Added Jess' bugfix to the forms, so they are clickable again.

--- views/blogPost/createBlogPost.php
+++ views/blogPost/createBlogPost.php
@@ -1,15 +1,15 @@
 <form action="index.php?controller=blogPost&action=save" method="POST">
     <div class="form-label-group">
         <label for="Title">Post Title:</label>
-        <input class="form-control" type="text" name="Title" autofocus="true" required>
+        <input class="form-control" id="Title" type="text" name="Title" autofocus="true" required />
     </div>
     <div class="form-label-group">
         <label for="Content">Blog Post Content</label>
-        <textarea name="Content" class="form-control" required></textarea>
+        <textarea name="Content" id="Content" class="form-control" required /></textarea>
     </div>
     <div class="form-label-group">
         <label for="Keywords">Keywords</label>
-        <input class="form-control" type="text" name="Keywords" placeholder="Add keywords, seperated by a comma">
+        <input class="form-control" id="Keywords" type="text" name="Keywords" placeholder="Add keywords, seperated by a comma">
     </div>
     <button class="loginButton form-control hvr-fade" type="sumbit" name="Submit">Submit</button>
 </form>
