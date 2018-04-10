ea547c5ec5ee3babc8abdfbc7e3c94da99f78ca4
ClareDavidson <clare@davidson.me.uk>
2018-04-10 11:09:38 +0100

    Fixed image uploading, updated permissions to the userImage folder and updated gitignore file to ignore files (except keep) in userImages

--- controllers/image_controller.php
+++ controllers/image_controller.php
@@ -1,49 +1,25 @@
 <?php
- class imageController {
-     
+class imageController {
+    const imageUploadKey = 'myfile';
     public function upload(){   
         require_once('views/blogPost/uploadImage.php');
     }
     
     public function save(){
-        require_once('models/image.php');
-        //$req->bindParam(':name', $name);
-        if(isset($_POST['name'])&& $_POST['name']!=""){
-            $filteredName = filter_input(INPUT_POST,'name', FILTER_SANITIZE_SPECIAL_CHARS);
+        try{
+            if(!isset($_POST['Submit'])){
+                throw new Exception('Not a form submission');
+            }
+            $_POST['imageFileName'] = $_FILES[self::imageUploadKey]['name'];
+            filter_input(INPUT_POST,'imageFileName', FILTER_SANITIZE_SPECIAL_CHARS);
+            require_once('models/image.php');
+            if(!Image::uploadFile($_POST['imageFileName'])){
+                throw new Exception('Image upload failed');
+            }
+            $image = new image($_POST['imageFileName']);
+        } catch (Exception $ex) {
+            echo "File upload error: ".$ex->getMessage();
+            require_once('views/blogPost/uploadImage.php');
         }
-        $imageName = $filteredName;
-        //$req->execute();
-        Image::uploadFile($imageName);
-
-        $image = new image();
     }
-    
-//    public function save(){
-//        const InputKey = 'myfile';
-//        const AllowedTypes = ['image/jpg', 'image/jpeg', 'image/png', 'image/gif'];            
-//        if (empty($_FILES[InputKey])) {
-//                echo "Please attach a file to upload.";
-//            }
-//            if ($_FILES[InputKey]['error'] > 0) {
-//                return call('pages', 'error');
-//            }
-//            if (!in_array($_FILES[InputKey] ['type'], AllowedTypes)) {
-//                echo "Please upload .jpg, .jpeg, .png or .gif files only.";
-//            }
-//            
-//            $tmpFile = $_FILES[InputKey]['tmp_name'];
-//            $dstFile = 'images/userImages/'.$_FILES[InputKey]['name'];
-//            
-//            if (!move_uploaded_file($tmpFile, $dstFile)){
-//                    die("Failed to move file.");
-//                }
-//            
-//           if (file_exists($tmpFile)) {
-//               unlink($tmp);
-//           }
-//     }
-     
-     
-     
-     
  }
--- models/image.php
+++ models/image.php
@@ -1,9 +1,9 @@
 <?php
 
 class Image {
-    private $imgName;
+    private $imageName;
     
-    public function __construct($imgName){
+    public function __construct($imageName){
         $this->imageName    = $imageName;
     }
     
@@ -11,49 +11,34 @@ class Image {
 //Product::uploadFile($name);
 //    }
 
-    const AllowedTypes = ['image/jpeg', 'image/jpg'];
-    const InputKey = 'myUploader';
+    const AllowedTypes = ['image/jpg', 'image/jpeg', 'image/png', 'image/gif'];
+    const InputKey = 'myfile';
     
     public function uploadFile($imageName){
-//        if(isset($_POST['name'])&& $_POST['name']!=""){
-//            $filteredName = filter_input(INPUT_POST,'name', FILTER_SANITIZE_SPECIAL_CHARS);
-//        }
-//        $name = $filteredName;
-//        $req->execute();
-
-//upload product image
-        try{
-            if (empty($_FILES[self::InputKey])) {
-                    //die("File Missing!");
-                    trigger_error("File Missing!");
-            }
-
-            if ($_FILES[self::InputKey]['error'] > 0) {
-                    trigger_error("Handle the error! " . $_FILES[InputKey]['error']);
-            }
-
-
-            if (!in_array($_FILES[self::InputKey]['type'], self::AllowedTypes)) {
-                    trigger_error("Handle File Type Not Allowed: " . $_FILES[self::InputKey]['type']);
-            }
-
-            $tempFile = $_FILES[self::InputKey]['tmp_name'];
-            $path = "/Applications/XAMPP/xamppfiles/htdocs/FinalProject/views/images/userImages";
-            $destinationFile = $path . $imageName . '.jpeg';
-
-            if (!move_uploaded_file($tempFile, $destinationFile)) {
-                    trigger_error("Handle Error");
-            }
-
-            //Clean up the temp file
-            if (file_exists($tempFile)) {
-                    unlink($tempFile); 
-            }
-
-            return true;
-        } catch (Exception $ex) {
-            echo $ex->getMessage().PHP_EOL;
+        if (empty($_FILES[self::InputKey])) {
+            throw new Exception("File Missing!");
         }
+        if ($_FILES[self::InputKey]['error'] > 0) {
+            throw new Exception("Handle the error! " . $_FILES[InputKey]['error']);
+        }
+        if (!in_array($_FILES[self::InputKey]['type'], self::AllowedTypes)) {
+            throw new Exception("Handle File Type Not Allowed: " . $_FILES[self::InputKey]['type']);
+        }
+
+        $tempFile = $_FILES[self::InputKey]['tmp_name'];
+        $path = "./views/images/userImages/";
+        $destinationFile = $path . $imageName;
+
+        if (!move_uploaded_file($tempFile, $destinationFile)) {
+            throw new Exception("Handle Error");
+        }
+
+        //Clean up the temp file
+        if (file_exists($tempFile)) {
+            unlink($tempFile); 
+        }
+
+        return true;
     }
     
 }
