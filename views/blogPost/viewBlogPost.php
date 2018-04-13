e23b040ba67f3d6e9570be6b6f7a2f3fa4a93b3c
JEF518 <36102590+JEF518@users.noreply.github.com>
2018-04-13 20:15:50 +0100

    Styled the comments
    Styled how comments display and the form to insert comments. Also amended how the date and time stamps the comments.

--- views/blogPost/viewBlogPost.php
+++ views/blogPost/viewBlogPost.php
@@ -2,28 +2,37 @@
 <p><?php echo $blogPost->getContent(); ?></p>
 <p><b><?php echo "Posted on: ". $blogPost->getDate(); ?></b></p>
 <p><?php echo "Keywords: ".$blogPost->getKeywords(); ?></p>
-<?php
+
+<hr id="style1">
+<p><b>Comments:</b></p> 
+    <?php
     foreach($blogPost->getComments() as $comment) {?>
-        <h3><?php echo $comment->getUsername(); ?></h3>
-        <p><?php echo $comment->getContent(); ?></p><?php
+<h5 align="left"><b><?php echo $comment->getUsername(); ?></b></h5>
+        <p><?php echo $comment->getContent(); ?></p><br>
+        <h5><?php echo $comment->getTimeCommented() . " | " . $comment->getDateCommented()?></h5>    
+        <hr>   <?php
     } ?>
 
 
+        <hr> 
 
-<h2>Leave a Comment</h2>
+<div class="col-sm-8" id="commentForm" style="height:auto; margin: 0 auto; align:center;">
 <form action="index.php?controller=comment&action=saveComment" method="POST">
+    <h4>Leave a Comment</h4><br>
     <input type="hidden" name="blogPostID" value="<?php echo $blogPost->getID();?>" />
-    <div class="form-label-group">
-        <input class="form-control" id="Title" type="text" name="username" autofocus="true" required />
-        <label for="username">Username:</label>
+    <div class="form-group">
+        <input class="form-control"type="text" name="username" placeholder="Username" required />
+<!--        <label for="username">Username:</label>-->
     </div>
-    <div class="form--label-group">
-        <div class="col-sm-8"><input type="email"  class="form-control" maxlength="50" name="userEmail" placeholder="Type a valid e-mail address" required/> </div>
-        <label class="control-label col-sm-2" for="userEmail">Email: </label>     
-     </div>
-    <div class="form-label-group">
-        <label for="content">Comment</label><br /><br />
-        <textarea name="content" id="content" class="form-control" required /></textarea>
+    <div class="form-group">
+        <input type="email"  class="form-control" maxlength="50" name="userEmail" placeholder="E-mail" required />
+<!--        <label class="control-label col-sm-2" for="userEmail">Email: </label>     -->
+    </div><br>
+    <div class="form-group">
+<!--        <label for="content">Comment</label><br /><br />-->
+        <textarea name="content" id="content" class="form-control" placeholder="Comment" required /></textarea>
     </div>
         <button class="loginButton form-control hvr-fade" type="submit" name="Submit">Submit</button>
 </form>
+    </div>
+
