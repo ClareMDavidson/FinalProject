b0f4cb0340079ddfa87f74d801f7ad7149c2d95a
ClareDavidson <clare@davidson.me.uk>
2018-04-14 12:07:59 +0100

    put div class styling on home.php

--- views/pages/home.php
+++ views/pages/home.php
@@ -1,8 +1,10 @@
-<?php
+
+<div class="blogPost">
+    <?php
     foreach($multiPost->getMultiPost() as $posts) {?>
         <h2><?php echo $posts->getTitle(); ?></h2>
         <p><?php echo substr($posts->getContent(), 0, 300).'...<a href="http://localhost/FinalProject/index.php?controller=blogPost&action=view&blogPostID='.$posts->getID().'">CLICK TO READ MORE</a>'; ?></p>
         <p><b><?php echo "Posted on: ".$posts->getDate(); ?></b></p>
         <p><?php echo "Keywords: ".$posts->getKeywords(); ?></p><?php
-    } 
- 
+    }?> 
+</div>
