437c1345778dfe5f8af6483b63946f613b67ae86
Jess Pearson <jessica.pearson@sky.uk>
2018-04-14 18:18:42 +0100

    Started making dropdown for archive

--- views/layout.php
+++ views/layout.php
@@ -23,8 +23,15 @@
                 <li class="nav-item"><a class="nav-link" href='?controller=pages&action=about'>About</a></li></h2>
             <h2 text-decoration="none" color="#F8FFF4">
                 <li class="nav-item"><a class="nav-link" href='#'>Recent Posts</a></li></h2>
-            <h2 text-decoration="none" color="#F8FFF4">
-                <li class="nav-item"><a class="nav-link" href='#'>Archive</a></li></h2>
+                <div class="dropdown" >
+                <h2 text-decoration="none" color="#F8FFF4">
+                    <li class="nav-item"><a class="nav-link" href="#">Archive</a></li>
+                </h2>
+                    <div class="dropdown-content">
+                        <a href="#">January</a>
+                        <a href="#">February</a>
+                    </div>
+            </div>
             <h2 text-decoration="none" color="#F8FFF4">
                 <li class="nav-item"><a class="nav-link" href='?controller=pages&action=contact'>Get in Touch</a></li></h2>
             <h2 text-decoration="none" color="#F8FFF4">
