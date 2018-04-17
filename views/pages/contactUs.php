8581659d5618b30b23224b6095e0c8d2014f17d8
ClareDavidson <clare@davidson.me.uk>
2018-04-17 18:22:09 +0100

    Added action to routes and pages controller. Started linking social media images.

--- views/pages/contactUs.php
+++ views/pages/contactUs.php
@@ -1,12 +1,4 @@
-<?php session_start(); ?>
-<!DOCTYPE html>
-<html>
-    <head>
-        
-        <title>Contact Us</title>
-        <h2>Contact Us</h2>
-
-<form action="/action_page.php">
+<form action="">
 
  <h3>Please leave your query, we will get back to you as soon as we can, Thank you</h3>
     
@@ -32,11 +24,3 @@ Closes 7PM<br>
 Phone: 0113 243 0111<br><br>
 Map:<br> 
 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d37697.22539479486!2d-1.5707575909405118!3d53.805920891774264!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48793e4ada64bd99%3A0x51adbafd0213dca9!2sLeeds!5e0!3m2!1sen!2suk!4v1523809690987" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
-
-
-
-
-</body>
-</html>
-</head>
-</html>
