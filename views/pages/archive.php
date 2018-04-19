90e232addb5151e7fa98fac64322d04bfd7bb761
Jess Pearson <jessica.pearson@sky.uk>
2018-04-19 19:54:43 +0100

    Wasn't actually working. Now works

--- views/pages/archive.php
+++ views/pages/archive.php
@@ -13,7 +13,7 @@ if (empty($posts)) {
     <div class="blogPost">
         <?php foreach ($archivePost->getArchivePost() as $posts) { ?>
         
-        <div class="blogLink"><h2><a href="http://localhost/FinalProject/index.php?controller=blogPost&action=view&blogPostID="<?php echo $posts->getID();?>><?php echo $posts->getTitle();?></a></h2></div>                            
+        <div class="blogLink"><h2><a href="http://localhost/FinalProject/index.php?controller=blogPost&action=view&blogPostID=<?php echo $posts->getID();?>"><?php echo $posts->getTitle();?></a></h2></div>                            
             <p><?php echo $posts->getContent(); ?></p>
             <p><b><?php echo "Posted on: " . $posts->getDate(); ?></b></p>
             <p><?php echo "Keywords: " . $posts->getKeywords(); ?></p>
