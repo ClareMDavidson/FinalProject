866797e77df52fb43dbb3b8bbc0447bad6d421a8
Jess Pearson <jessica.pearson@sky.uk>
2018-04-21 17:55:13 +0100

    started register class

--- models/register.php
+++ models/register.php
@@ -2,7 +2,12 @@
 
 class Register {
     
-    
+    public static function registerContributor () {
+        if (isset($_POST['username'])&&(isset($_POST['password']))) {
+            $filteredUsername = filter_input(INPUT_POST,'username', FILTER_SANITIZE_SPECIAL_CHARS);
+            $filteredPassword = filter_input(INPUT_POST,'password', FILTER_SANITIZE_SPECIAL_CHARS);
+        }
+    }
     
     
     
