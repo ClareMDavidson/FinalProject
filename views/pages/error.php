5bde26d8788af8c44507929697660f0a02bc6db7
GayathriThangavelu <36862501+GayathriThangavelu@users.noreply.github.com>
2018-04-21 09:48:40 +0100

    error.php
    changed image size and changed code

--- views/pages/error.php
+++ views/pages/error.php
@@ -1,31 +1,5 @@
-<?php session_start(); ?>
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-        <!--link to bootstrap cdn-->
-        <link rel="stylesheet" href="https://cdn.rawgit.com/tonystar/bootstrap-float-label/v4.0.0/dist/bootstrap-float-label.min.css"/>
-        <!--link to bootstrap floating labels-->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Amatic+SC|Montserrat|Open+Sans">
-        <!--direct link to google fonts-->
-        <link rel="stylesheet" href="views/css/styles.css">
-        <!--link to internal style sheet-->
-        <script type="text/javascript" src="//platform-api.sharethis.com/js/sharethis.js#property=5ad09e3883e6290013a1c2d5&product=inline-share-buttons"></script>
 
-<title>ERROR PAGE</title>
-    </head>
-    <body>
-        <p>Oops, We're sorry but the page you're looking for could not be found.</p>
+        <h2>Oops!! It looks like something went wrong, please try again.</h2>
         
         
-        <img src="../images/error.png" alt=""/>
-
-        <p>It looks like something went wrong, Please Try again.:(</p>
-
-    
-       
-    </body>
-</html>
-
+        <img src="views/images/error.png" alt="error" width="40%"/>
