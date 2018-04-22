b7aa6b79652332571f04c99f5b3751466db1cd35
ClareDavidson <clare@davidson.me.uk>
2018-04-22 22:50:29 +0100

    Added h2 styling to the comment thank you view and comment moderation view

--- views/blogPost/commentThankYou.php
+++ views/blogPost/commentThankYou.php
@@ -1,4 +1,4 @@
 <div class="blogPost">
-    <h3>Thank your for leaving a comment, it is currently awaiting moderation.</h3>
-    <h3>Return to <a href="index.php?controller=blogPost&action=view&blogPostID=<?php echo $_POST['blogPostID'];?>">blog post</a>.</h3>
+    <h2>Thank your for leaving a comment, it is currently awaiting moderation.</h2>
+    <h2>Return to <a class="blogLink" href="index.php?controller=blogPost&action=view&blogPostID=<?php echo $_POST['blogPostID'];?>">blog post</a>.</h2>
 </div>
